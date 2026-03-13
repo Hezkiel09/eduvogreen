d8d386e4d8bb7048463b9d582ba5b1fa6683226b
Hezkiel09 <kielsiahaan09@gmail.com>
2026-03-13 13:39:35 +0700

    splash screen, app logo

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,6 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await SupabaseConfig.init();
   // cek session
-  
   final session = Supabase.instance.client.auth.currentSession;
   final isLoggedIn = session != null;
 
